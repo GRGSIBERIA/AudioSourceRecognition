8041c77d2079e286fdc35ca274a218dff00ef41c
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-01-13 21:39:02 +0900

    ドロップダウンを選択するとマイクのチャンネル数を取得する

--- AudioSourceRecognition/Assets/Scripts/DeviceManager.cs
+++ AudioSourceRecognition/Assets/Scripts/DeviceManager.cs
@@ -6,7 +6,12 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Dropdown))]
 public class DeviceManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject channelDropdownObject;
+
     Dropdown drop;
+
+    Dropdown channel;
 
     /// <summary>
     /// マイクに使えるデバイスリスト
@@ -17,7 +22,9 @@ public class DeviceManager : MonoBehaviour
     void Start()
     {
         drop = GetComponent<Dropdown>();
+        channel = channelDropdownObject.GetComponent<Dropdown>();
 
+        // オプションにデバイスの名前を登録する
         foreach (var name in Microphone.devices)
         {
             var datum = new Dropdown.OptionData();
@@ -27,5 +34,26 @@ public class DeviceManager : MonoBehaviour
         }
 
         drop.options = devices;
+
+        // ドロップダウンが変更されたとき、自動的にチャンネル数を取得する
+        drop.onValueChanged.AddListener((int id) =>
+        {
+            var clip = Microphone.Start(drop.options[id].text, false, 1, 44100);
+            var channels = new List<Dropdown.OptionData>();
+
+            // 指定されたデバイスのチャンネル数を登録する
+            // 8チャンネルデバイスでも何チャンネルを使用するか指定できる
+            // ただし、調べるとUnityのMicrophoneはモノラル録音しか対応していない様子
+            for (int i = 0; i < clip.channels; ++i)
+            {
+                var datum = new Dropdown.OptionData();
+                datum.text = (i + 1).ToString();
+                datum.image = null;
+                channels.Add(datum);
+            }
+
+            channel.options = channels;
+            Microphone.End(drop.options[id].text);
+        });
     }
 }
