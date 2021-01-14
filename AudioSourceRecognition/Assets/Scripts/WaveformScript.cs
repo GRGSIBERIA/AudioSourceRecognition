f6b7b1bda39ed1e18b3f72d777264ea2b2da811b
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-01-14 23:52:40 +0900

    音響信号の波形を取得できた

--- AudioSourceRecognition/Assets/Scripts/WaveformScript.cs
+++ AudioSourceRecognition/Assets/Scripts/WaveformScript.cs
@@ -15,17 +15,19 @@ public class WaveformScript : MonoBehaviour
 
     bool initialized = false;
 
-    Resolution prevResolution;
+    int pvWidth;
+    int pvHeight;
 
     // Start is called before the first frame update
     void Start()
     {
         recorder = recordManager.GetComponent<RecordManager>();
         line = GetComponent<LineRenderer>();
-        line.startWidth = 0.1f;
-        line.endWidth = 0.1f;
-        prevResolution = Screen.currentResolution;
-        CleanAspect(prevResolution);
+        line.startWidth = 0.01f;
+        line.endWidth = 0.01f;
+        pvWidth = Screen.width;
+        pvHeight = Screen.height;
+        CleanAspect(pvWidth, pvHeight);
     }
 
     private void Update()
@@ -38,29 +40,16 @@ public class WaveformScript : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// ユークリッド互除法
-    /// </summary>
-    /// <param name="x">長辺</param>
-    /// <param name="y">短辺</param>
-    /// <returns>公約数</returns>
-    int gcd(int x, int y)
-    {
-        if (y == 0) return x;
-        return gcd(y, x % y);
-    }
-
     [SerializeField]
     float aspectX;
 
     [SerializeField]
     float aspectY;
 
-    void CleanAspect(Resolution resolution)
+    void CleanAspect(int width, int height)
     {
-        float match = gcd(resolution.width, resolution.height);
-        aspectX = resolution.width / match;
-        aspectY = resolution.height / match;
+        aspectX = 5;
+        aspectY = 5;
     }
 
     // Update is called once per frame
@@ -72,17 +61,11 @@ public class WaveformScript : MonoBehaviour
 
         var waveform = recorder.GetData();
 
-        // 解像度に変更が加えられた場合の処理
-        if (!(prevResolution.width == resolution.width && prevResolution.height == resolution.height))
-        {
-            CleanAspect(resolution);
-        }
+        // アスペクト比は固定で行く
 
-        float aspect = (float)resolution.width / (float)resolution.height;
-        
-        float offset = aspectY;
-        float xdiff = aspectY * 2f / (float)waveform.Length;
-        float ydiff = (float)aspectX * 0.25f / Mathf.Pow(2f, 24f);
+        float offset = aspectX;
+        float xdiff = aspectX * 2f / (float)waveform.Length;
+        float ydiff = 0.5f;
 
         for (int i = 0; i < waveform.Length; ++i)
         {
@@ -93,6 +76,5 @@ public class WaveformScript : MonoBehaviour
 
         line.SetPositions(vertices);
 
-        prevResolution = resolution;
     }
 }
