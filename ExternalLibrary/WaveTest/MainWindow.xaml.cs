c657f10c1d81bfa88a9b3dd0e4935ec9a87f5624
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-01-18 21:31:29 +0900

    ぜんぜんわからんので削除する

--- ExternalLibrary/WaveTest/MainWindow.xaml.cs
+++ ExternalLibrary/WaveTest/MainWindow.xaml.cs
@@ -8,13 +8,15 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
+//using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Collections.ObjectModel;
-using Sparrow;
-using Sparrow.Chart;
+//using Sparrow;
+//using Sparrow.Chart;
+using OxyPlot;
+using OxyPlot.Axes;
 
 namespace WaveTest
 {
@@ -23,18 +25,37 @@ namespace WaveTest
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<Point> Waveforms { get; private set; }
-        public ObservableCollection<Point> Spectrums { get; private set; }
-
+        public PlotModel WaveformModel { get; private set; }
+        public PlotModel SpectrumModel { get; private set; }
+        
         public MainWindow()
         {
             InitializeComponent();
 
-            
+            this.DataContext = this;
+
+            WaveformModel = new PlotModel();
+            WaveformModel.Title = "test";
+
+            var axisX = new LinearAxis { Position = AxisPosition.Bottom, Title="hoge" };
+            WaveformModel.Axes.Add(axisX);
+
+            var axisY = new LinearAxis { Position = AxisPosition.Left, Title="power" };
+            WaveformModel.Axes.Add(axisY);
+
+            WaveformChart.Model = WaveformModel;
+
+            SpectrumModel = new PlotModel();
         }
 
         private void generateButton_Click(object sender, RoutedEventArgs e)
         {
+            float time = float.Parse(timeBox.Text);
+            int samplingRate = int.Parse(samplingRateBox.Text);
+            float frequency = float.Parse(sinWaveBox.Text);
+
+            float dt = 1f / (float)samplingRate;
+            int totalSample = (int)(time / dt);
 
         }
 
