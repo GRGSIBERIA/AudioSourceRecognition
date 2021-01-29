a8202c9609ad1a59d32de54f914a9d4a9f6e4fcd
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-01-29 16:19:09 +0900

    ベクトル計算を行う

--- GuitarRecognition/GuitarRecognition/Analyzer.hpp
+++ GuitarRecognition/GuitarRecognition/Analyzer.hpp
@@ -2,6 +2,8 @@
 #include <otfft.h>
 #include <vector>
 #include <omp.h>
+#include <Siv3D.hpp>
+#include <intrin.h>
 #include "TinyASIO/TinyASIO.hpp"
 using OTFFT::complex_t;
 using OTFFT::simd_malloc;
@@ -10,6 +12,7 @@ using OTFFT::simd_free;
 class Analyzer
 {
 	std::vector<complex_t*> outputs;
+	std::vector<double*> results;
 	std::vector<OTFFT::FFT> ffts;
 
 	complex_t* workspace = nullptr;
@@ -32,6 +35,7 @@ class Analyzer
 		for (int i = 0; i < stride; ++i)
 		{
 			allocation(N >> i);
+			results.push_back((double*)simd_malloc(N >> i * sizeof(double)));
 		}
 		workspace = (complex_t*)simd_malloc(sizeof(complex_t) * N);
 	}
@@ -70,6 +74,45 @@ public:
 		simd_free(workspace);
 	}
 
+private:
+	void computeAveragePowerSpectrums(const size_t st, const size_t sl)
+	{
+		const double invSlide = 1.0 / slide;
+		const double invs[2] = { invSlide, invSlide };
+
+		complex_t* rv = outputs[st * stride + sl];
+		auto& average = results[st];
+
+		// 答えの配列をゼロで初期化する
+		memset(&results[st], 0.0, sizeof(complex_t) * (N >> st));
+
+		// intrinsicな感じでパワースペクトルを求める
+		for (size_t ni = 0; ni < (N >> st); ++ni)
+		{
+			// 意訳すると以下の処理をしたいだけ
+			// double d = 0.0;
+			//d = rv[ni].Im * rv[ni].Im + rv[ni].Re * rv[ni].Re;
+			//d = Math::Sqrt(d);
+			//d = d * d / inv;
+			//average[ni] += d;
+
+			__m128d d = _mm_load_pd((double*)&rv[ni]);
+			__m128d inv = _mm_load_pd((double*)invs);
+
+			// 二乗平方根を取る
+			d = _mm_sqrt_pd(_mm_dp_pd(d, d, 0b11111111));
+			d = _mm_mul_pd(d, d);
+			d = _mm_mul_pd(d, inv);	//A * (x + y + z) = Ax + Ay + Azに展開してもよい
+			
+			// 加算平均を取る
+			__m128d avg = _mm_load_pd((double*)&average[ni]);
+			d = _mm_add_pd(d, avg);
+
+			_mm_storel_pd(&average[ni], d);
+		}
+	}
+
+public:
 	void fft(const std::vector<asio::SampleType>& samples)
 	{
 		// intをdoubleに変換する
@@ -102,6 +145,15 @@ public:
 				ffts[st * stride + sl].fwd(outputs[st * stride + slide]);
 			}
 		}
-			
+
+		// 平均スペクトルを計算する
+#pragma omp parallel for
+		for (size_t st = 0; st < stride; ++st)
+		{
+			for (size_t sl = 0; sl < slide; ++sl)
+			{
+				computeAveragePowerSpectrums(st, sl);
+			}
+		}
 	}
 };
