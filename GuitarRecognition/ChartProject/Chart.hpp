3b53e811636ee61aec82baf911c33a80b8cecf37
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-01-30 17:05:47 +0900

    チャート描画用のプロジェクトを作成

---
+++ GuitarRecognition/ChartProject/Chart.hpp
@@ -0,0 +1,62 @@
+﻿#include <Siv3D.hpp>
+
+
+
+class Chart
+{
+	const Font& font;
+	RectF region;
+
+	String title;
+	String xlabel;
+	String ylabel;
+
+	Texture titletex;
+	Texture xtex;
+	Texture ytex;
+
+	Image titleimg;
+	Image ximg;
+	Image yimg;
+
+private:
+	void initializeLabel(String& basename, const String& setname, Texture& tex, Image& img)
+	{
+		basename = setname;
+		const auto region = font(basename).region();
+		img.resize(region.w, region.h);
+		font(basename).overwrite(img);
+		tex = Texture(img);
+	}
+
+public:
+	Chart(const Font& font, const RectF& region)
+		: font(font), region(region) {}
+
+	Chart(const Font& font, const RectF& region, const String& title, const String& xlabel, const String& ylabel)
+		: font(font), region(region) 
+	{
+		setTitle(title);
+		setXLabel(xlabel);
+		setYLabel(ylabel);
+	}
+
+	void setTitle(const String& title) 
+	{ 
+		initializeLabel(this->title, title, titletex, titleimg);
+	}
+	void setXLabel(const String& xlabel) 
+	{ 
+		initializeLabel(this->xlabel, xlabel, xtex, ximg);
+	}
+	void setYLabel(const String& ylabel) 
+	{ 
+		initializeLabel(this->ylabel, ylabel, ytex, yimg);
+	}
+
+	Chart& draw(const Vec2& pos)
+	{
+		
+	}
+
+};
---
+++ GuitarRecognition/ChartProject/Main.cpp
@@ -0,0 +1,19 @@
+﻿
+#include <Siv3D.hpp> // OpenSiv3D v0.4.3
+#include "Chart.hpp"
+
+void Main()
+{
+	// 背景を水色にする
+	Scene::SetBackground(ColorF(0.8, 0.9, 1.0));
+
+	// 大きさ 60 のフォントを用意
+	const Font font(24);
+
+	Chart chart(font);
+
+	while (System::Update())
+	{
+		
+	}
+}
